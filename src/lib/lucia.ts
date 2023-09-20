0c25c2f7c3d1b1c6540ddb089a8b7fd88badb826
MrEpoch <alexandrstencuk@seznam.cz>
2023-09-20 14:59:15 +0200

    Added redis database for managing sessions

--- src/lib/lucia.ts
+++ src/lib/lucia.ts
@@ -1,13 +1,18 @@
 import { lucia } from 'lucia';
 import { prisma as client } from './db';
 import { prisma } from '@lucia-auth/adapter-prisma';
+import { ioredis } from "@lucia-auth/adapter-session-redis";
+import redisClient from "./redis";
 import { sveltekit } from 'lucia/middleware';
 import { dev } from '$app/environment';
 import { github } from '@lucia-auth/oauth/providers';
 import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
 
 export const auth = lucia({
-	adapter: prisma(client),
+    adapter: {
+        user: prisma(client),
+        session: ioredis(redisClient)
+    },
 	env: dev ? 'DEV' : 'PROD',
 	middleware: sveltekit(),
 	getUserAttributes: (data) => {
--- src/lib/order.ts
+++ src/lib/order.ts
@@ -1,3 +1,4 @@
+import { dev } from '$app/environment';
 import { STRIPE_SECRET_KEY } from '$env/static/private';
 import { prisma } from './db';
 import Stripe from 'stripe';
@@ -48,8 +49,8 @@ export const makeOrder = async (
 			{
 				line_items: orders,
 				mode: 'payment',
-				success_url: 'https://www.tomatodream.store/payment/success',
-				cancel_url: 'https://www.tomatodream.store/payment/cancel'
+                success_url: dev ? 'https://www.tomatodream.store/payment/success' : 'http://localhost:5173/payment/success',
+                cancel_url: dev ? 'https://www.tomatodream.store/payment/cancel' : 'http://localhost:5173/payment/cancel',
 			},
 			{
 				apiKey: STRIPE_SECRET_KEY
